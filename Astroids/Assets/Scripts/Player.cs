f7ec420794635e33a69f834a1f816a9914aa57bb
Sebzzz69 <callenholmljungsebastian@gmail.com>
2022-05-08 18:19:15 +0200

    Player Movement

---
+++ Astroids/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float thrustSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+
+    private Rigidbody2D rigidbody2d;
+    private bool _thrusting;
+    private float _turnDirection;
+
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _turnDirection = 1.0f;
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            _turnDirection = -1.0f;
+        } else
+        {
+            _turnDirection = 0.0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_thrusting)
+        {
+            rigidbody2d.AddForce(this.transform.up * this.thrustSpeed);
+        }
+        if (_turnDirection != 0.0f)
+        {
+            rigidbody2d.AddTorque(_turnDirection * this.turnSpeed);
+        }
+    }
+
+}
