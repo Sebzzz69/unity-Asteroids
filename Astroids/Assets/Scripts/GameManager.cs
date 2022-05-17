79feff964534d4f29433d2bf71a6caacae62bc60
Sebzzz69 <callenholmljungsebastian@gmail.com>
2022-05-17 19:16:38 +0200

    Particles and Game over

--- Astroids/Assets/Scripts/GameManager.cs
+++ Astroids/Assets/Scripts/GameManager.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Player player;
 
+    public ParticleSystem explotion;
+
     public float respawnTime = 3.0f;
     public float respawnInvunlnerabilityTime = 3.0f;
 
     public int lives = 3;
+    public int score = 0;
 
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void AsteroidDestroyed(Asteroid asteroid)
+    {
+        //Particle effect
+        this.explotion.transform.position = asteroid.transform.position;
+        this.explotion.Play();
+
+        if (asteroid.size < 0.75f) {
+            score += 100;
+        } else if (asteroid.size < 1.2f) {
+            score += 50;
+        } else {
+            this.score += 25;
+        }
+    }
+     
     public void PlayerDied()
     {
+        this.explotion.transform.position = this.player.transform.position;
+        this.explotion.Play();
+
         this.lives--;
 
         if (this.lives <= 0){
@@ -20,6 +47,11 @@ public class GameManager : MonoBehaviour
             Invoke(nameof(Respawn), respawnTime);
         }
 
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("Asteroids");
     }
 
     private void Respawn()
@@ -38,7 +70,12 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
-        // TODO
+        this.lives = 3;
+        this.score = 0;
+
+        Invoke(nameof(Respawn), respawnTime);
+        ReloadScene();
+        
     }
 
 }
